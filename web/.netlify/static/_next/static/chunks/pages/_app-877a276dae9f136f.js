084bbd812da84e66040eff9e9a3c69d5e9e2be78
Folk <jirakit.c@terradigitalventures.com>
2025-08-25 11:40:18 +0700

    add netlify account

---
+++ web/.netlify/static/_next/static/chunks/pages/_app-877a276dae9f136f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{44589:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(87308)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(44589),n(50346))),_N_E=_.O()}]);
